b50df5ac4c1fa85e77d81cc595ca94af25857482
gonzalosovero13 <gonzalosov.135ns@gmail.com>
2025-06-18 01:52:10 -0500

    cambios comida aun no sale

--- AutoServicioCineWeb/Form.Master.designer.cs
+++ AutoServicioCineWeb/Form.Master.designer.cs
@@ -69,6 +69,24 @@ namespace AutoServicioCineWeb
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl tituloSpan;
 
         /// <summary>
+        /// Control litFecha.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litFecha;
+
+        /// <summary>
+        /// Control litHora.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal litHora;
+
+        /// <summary>
         /// Control TicketsSummary.
         /// </summary>
         /// <remarks>
